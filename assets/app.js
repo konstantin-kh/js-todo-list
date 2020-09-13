c5b5f4bdfa86effd1c3e8fd35dbac3086f4ffdbf
Konstantin Pomazan <kospomazan@gmail.com>
2020-09-13 19:05:41 +0300

    added done function

--- assets/app.js
+++ assets/app.js
@@ -1,51 +1,73 @@
+function onPageLoaded() {
+  const APP = document.querySelector('#app');
+  const APP_CONTAINER = document.createElement('div');
+  const INPUT = document.createElement('input');
+  const LIST = document.createElement('ul');
+  const BUTTON_ADD = document.createElement('button');
+  const TODO_LIST = [];
 
-const APP = document.querySelector('#app');
-const APP_CONTAINER = document.createElement('div');
-const INPUT = document.createElement('input');
-const LIST = document.createElement('ul');
-const BUTTON_ADD = document.createElement('button');
-const TODO_LIST = [];
-// const FRAGMENT = document.createDocumentFragment();
+  APP_CONTAINER.classList.add('container');
+  INPUT.setAttribute('type', 'text');
+  INPUT.setAttribute('placeholder', 'text');
+  BUTTON_ADD.setAttribute('type', 'button');
+  BUTTON_ADD.classList.add('btn');
+  BUTTON_ADD.textContent = 'Add task';
+  APP_CONTAINER.appendChild(INPUT);
+  APP_CONTAINER.appendChild(BUTTON_ADD);
+  APP_CONTAINER.appendChild(LIST);
+  APP.appendChild(APP_CONTAINER);
 
-APP_CONTAINER.classList.add('container');
-INPUT.setAttribute('type', 'text');
-INPUT.setAttribute('placeholder', 'text');
-BUTTON_ADD.setAttribute('type', 'button');
-BUTTON_ADD.classList.add('btn');
-BUTTON_ADD.textContent = 'Add task';
+  function addTodo(text) {
+    const TODO = {
+      text,
+      checked: false,
+      id: Date.now(),
+    };
 
-// FRAGMENT.appendChild(APP_CONTAINER);
-APP_CONTAINER.appendChild(INPUT);
-APP_CONTAINER.appendChild(BUTTON_ADD);
-APP_CONTAINER.appendChild(LIST);
-APP.appendChild(APP_CONTAINER);
+    LIST.insertAdjacentHTML(
+      'afterbegin',
+      `
+      <li data-key='${TODO.id}' id='${TODO.id}'>
+        <span class='trash' data-key='${TODO.id}'>
+          <i class='fas fa-trash'></i>
+        </span>
+        <span class='todo-text' data-key='${TODO.id}'>${TODO.text}</span>
+      </li>
+    `
+    );
 
-function addTodo(text) {
-  const TODO = {
-    text,
-    checked: false,
-    id: Date.now(),
-  };
+    TODO_LIST.push(TODO);
+  }
 
-  LIST.insertAdjacentHTML('beforeend', `
-    <li data-key="${TODO.id}" id="${TODO.id}">
-      <span class="trash" data-key="${TODO.id}">
-        <i class="fas fa-trash"></i>
-      </span>
-      <span class="todo-text">${TODO.text}</span>
-    </li>
-  `);
+  function toggleDone(key) {
+    const INDEX = TODO_LIST.findIndex(item => item.id === Number(key));
+    TODO_LIST[INDEX].checked = !TODO_LIST[INDEX].checked;
 
-  TODO_LIST.push(TODO);
-}
-
-INPUT.addEventListener('keypress', event => {
-  if (event.keyCode === 13) {
-    const TEXT = INPUT.value.trim();
-    if (TEXT !== '') {
-      addTodo(TEXT);
-      INPUT.value = '';
-      INPUT.focus();
+    const ITEM = document.querySelectorAll(`[data-key='${key}']`);
+    if (TODO_LIST[INDEX].checked) {
+      ITEM[2].classList.add('done');
+    } else {
+      ITEM[2].classList.remove('done');
     }
   }
-});
+
+  INPUT.addEventListener('keypress', event => {
+    if (event.key === 'Enter') {
+      const TEXT = INPUT.value.trim();
+      if (TEXT !== '') {
+        addTodo(TEXT);
+        INPUT.value = '';
+        INPUT.focus();
+      }
+    }
+  });
+
+  LIST.addEventListener('click', event => {
+    if (event.target.classList.contains('todo-text')) {
+      const ITEM_KEY = event.target.parentElement.dataset.key;
+      toggleDone(ITEM_KEY);
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', onPageLoaded);
